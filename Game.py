98a46aa7e9e2e674894aed2673fd5f473bb8a7f5
Matthew Brookes <mbrookes1304@gmail.com>
2016-04-24 18:16:46 +0000

    Create a stub Game class

---
+++ Game.py
@@ -0,0 +1,25 @@
+class Game(object):
+    '''Represents a game of Dudo.'''
+
+    def __init__(self, playername):
+        pass
+
+    def makeBid(self, bid):
+        '''Change the latest bid.'''
+        pass
+
+    def callBid(self):
+        '''The bid made by the previous player is called.'''
+        pass
+
+    def callSpotOn(self):
+        '''The player believes the last bid to be spot on.'''
+        pass
+
+    def nextTurn(self):
+        '''Play proceeds to the next player.'''
+        pass
+
+    def newRound(self, firstPlayer):
+        '''Start a new round from the given player.'''
+        pass
